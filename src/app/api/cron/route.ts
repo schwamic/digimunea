9880839f8c911889f5400dbabf677dcbd54d6c3c
schwamic <schwarz.michael@posteo.net>
2026-01-03 14:51:52 +0100

    feat(notifications): update

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest } from 'next/server';
 import prisma from '@src/lib/server/prisma';
 
+/**
+ * Cron Job to delete notifications older than 2 days every day at 1 AM ("0 1 * * *")
+ * https://vercel.com/digimuneas-projects/webseite/settings/cron-jobs
+ */
 export async function GET(request: NextRequest) {
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
