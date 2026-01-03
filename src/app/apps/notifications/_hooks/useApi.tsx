cafe70171f1698f54daef855ba5cd7b328dd6cbe
schwamic <schwarz.michael@posteo.net>
2026-01-03 21:11:03 +0100

    feat(notifications): update

--- src/app/apps/notifications/_hooks/useApi.tsx
+++ src/app/apps/notifications/_hooks/useApi.tsx
@@ -44,6 +44,7 @@ export default function useApi() {
         },
         onSuccess: () => {
             queryClient.setQueryData(['user'], null);
+            queryClient.clear();
         },
     });
 
